aaf7d07f1f1742de3c22f5995382b9a174f0606d
watagashi0619 <watagashi0619@gmail.com>
2023-11-11 12:20:12 +0900

    feat: add gpt4-vision and change to -1106

--- discord_bot/gpt/gptbot.py
+++ discord_bot/gpt/gptbot.py
@@ -1,5 +1,7 @@
+import base64
 import math
 import os
+import tempfile
 import tomllib
 import traceback
 from logging import config, getLogger
@@ -32,8 +34,10 @@ DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN_GPT")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 BOT_NAME = "chat-gpt"
 CHANNEL_ID = os.getenv("CHANNEL_ID_GPT")
-model_engine = "gpt-3.5-turbo"
-chat_log = [
+model_engine = "gpt-3.5-turbo-1106"
+# model_engines = ["gpt-3.5-turbo-1106","gpt-4-1106-preview","gpt-4-vision-preview"]
+chat_log = []
+system_set = [
     {
         "role": "system",
         "content": "あなたの名前は「ことのせ つむぐ」で、私をアシストしてくれる優しい女の子です。"
@@ -145,32 +149,27 @@ async def gpt_delete(interaction: discord.Interaction):
     """
     logger.info("command: gpt-hflush")
     global chat_log
-    chat_log = [
-        {
-            "role": "system",
-            "content": "あなたの名前は「ことのせ つむぐ」で、私をアシストしてくれる優しい女の子です。"
-            + "敬語や丁寧語、「ですます」調を一切使わずにタメ口で返答してください。"
-            + "タメ口とは、敬語や丁寧語を一切使わずに話すこと。文末の動詞や助詞を省略したり、体言止めを使ったりすることがよくあります。親しみやすさを出すために、くだけた表現やスラング、略語などが使われることがあります。",
-        }
-    ]
+    chat_log = []
     logger.info("Deleted chat logs.")
     response = "チャット履歴を削除しました。"
     await interaction.response.send_message(response)
 
 
-@tree.command(name="gpt-switch", description="chat gptモデルをgpt-3.5-turboとgpt-4の間で切り替える")
+@tree.command(name="gpt-switch", description="chat gptモデルをgpt-3.5-turbo-1106とgpt-4の間で切り替える")
 async def gpt_switch(interaction: discord.Interaction):
-    """switching the ChatGPT model between gpt-3.5-turbo and gpt-4.
+    """switching the ChatGPT model between gpt-3.5-turbo-1106 and gpt-4.
 
     Args:
         interaction (discord.Interaction): interaction.
     """
     logger.info("command: gpt-switch")
     global model_engine
-    if model_engine == "gpt-3.5-turbo":
-        model_engine = "gpt-4"
-    else:
-        model_engine = "gpt-3.5-turbo"
+    if model_engine == "gpt-3.5-turbo-1106":
+        model_engine = "gpt-4-1106-preview"
+    elif model_engine == "gpt-4-1106-preview":
+        model_engine = "gpt-4-vision-preview"
+    elif model_engine == "gpt-4-vision-preview":
+        model_engine = "gpt-3.5-turbo-1106"
     response = f"モデルエンジンを {model_engine} に変更しました。"
     logger.info("Change the model engine to " + model_engine)
     await interaction.response.send_message(response)
@@ -185,8 +184,14 @@ async def gpt_system(interaction: discord.Interaction, prompt: str):
         prompt (str): the setting of the ChatGPT character you want it to be.
     """
     logger.info("command: gpt-system")
-    global chat_log
-    chat_log.append({"role": "system", "content": prompt})
+    global system_set
+    # chat_log.append({"role": "system", "content": prompt})
+    system_set = [
+        {
+            "role": "system",
+            "content": prompt,
+        }
+    ]
     logger.info("Set gpt character.")
     response = "role: systemを次のように設定しました:\n" + ">>> " + prompt
     await interaction.response.send_message(response)
@@ -222,34 +227,67 @@ async def on_message(message):
             await msg.delete()
             await message.channel.send("質問内容がありません")
             return
-        chat_log.append({"role": "user", "content": prompt})
-        logger.info("user: " + prompt)
-
+        content = prompt
+        if len(message.attachments) > 0 and model_engine == "gpt-4-vision-preview":
+            for attachment in message.attachments:
+                if attachment.content_type.startswith("image"):
+                    # 画像のダウンロード
+                    image_data = await attachment.read()
+                    # 一時ファイルとして保存
+                    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                        temp_file.write(image_data)
+                    img_path = temp_file.name
+                    # base64
+                    with open(img_path, "rb") as image_file:
+                        base64_image = base64.b64encode(image_file.read()).decode("utf-8")
+                    content = [
+                        {"type": "text", "text": f"{prompt}"},
+                        {
+                            "type": "image_url",
+                            "image_url": f"data:image/jpeg;base64,{base64_image}",
+                        },
+                    ]
+        chat_log.append({"role": "user", "content": content})
+        logger.info(f"user: {content}")
         retries = 3
         while retries > 0:
             try:
-                completion = openai.ChatCompletion.create(model=model_engine, messages=chat_log, request_timeout=120)
+                input_chat_log = chat_log
+                if model_engine != "gpt-4-vision-preview":
+                    input_chat_log = system_set + chat_log
+                completion = openai.ChatCompletion.create(
+                    model=model_engine, messages=input_chat_log, request_timeout=120, max_tokens=2000
+                )
                 response = completion["choices"][0]["message"]["content"]
                 response_list = split_string(response)
                 chat_log.append(completion["choices"][0]["message"].to_dict())
                 logger.info("assistant: " + completion["choices"][0]["message"].to_dict()["content"])
-                if model_engine == "gpt-3.5-turbo":
+                if model_engine == "gpt-3.5-turbo-1106":
                     price = round_to_digits(
-                        completion["usage"]["prompt_tokens"] * 0.0015 / 1000
-                        + completion["usage"]["completion_tokens"] * 0.002 / 1000,
+                        completion["usage"]["prompt_tokens"] * 0.0010 / 1000
+                        + completion["usage"]["completion_tokens"] * 0.0020 / 1000,
                         3,
                     )
-                elif model_engine == "gpt-4":
+                elif model_engine == "gpt-4-1106-preview":
                     price = round_to_digits(
-                        completion["usage"]["prompt_tokens"] * 0.03 / 1000
-                        + completion["usage"]["completion_tokens"] * 0.06 / 1000,
+                        completion["usage"]["prompt_tokens"] * 0.01 / 1000
+                        + completion["usage"]["completion_tokens"] * 0.03 / 1000,
+                        3,
+                    )
+                    response_list.append(f"(USAGE: {price} USD)")
+                elif model_engine == "gpt-4-vision-preview":
+                    price = round_to_digits(
+                        completion["usage"]["prompt_tokens"] * 0.01 / 1000
+                        + completion["usage"]["completion_tokens"] * 0.03 / 1000,
                         3,
                     )
                     response_list.append(f"(USAGE: {price} USD)")
                 logger.info(f"Usage: {price} USD")
                 total_token += completion["usage"]["total_tokens"]
                 if total_token > 4096 - 256:
-                    chat_log = chat_log[:1] + chat_log[2:]
+                    chat_log = chat_log[1:]
+                logger.info(chat_log)
+                # logger.debug(completion)
                 await msg.delete()
                 for response in response_list:
                     await message.reply(response, mention_author=False)
@@ -262,7 +300,7 @@ async def on_message(message):
                 retries -= 1
                 logger.exception(e)
                 await reply_openai_exception(retries, message, e)
-                chat_log = chat_log[:1] + chat_log[2:]
+                chat_log = chat_log[1:]
             except discord.errors.HTTPException as e:
                 logger.exception(e)
                 await message.reply(
