1a68e3b08344edad7a62afe3b73c242b83914f29
watagashi0619 <watagashi0619@gmail.com>
2024-04-21 21:01:24 +0900

    feat(gptbot): Added paper summary functionality.
    If you give them a PDF of a paper, they can interact with a summary of the paper and learn about the content of the paper. The api content has also been updated to coincide with the release of the new version of GPT.

--- discord_bot/gpt/gptbot.py
+++ discord_bot/gpt/gptbot.py
@@ -1,6 +1,8 @@
 import base64
+import json
 import math
 import os
+import re
 import tempfile
 import tomllib
 import traceback
@@ -11,6 +13,7 @@ import discord
 import openai
 from discord import app_commands
 from dotenv import load_dotenv
+from pdfminer.high_level import extract_text
 
 current_folder_abspath = os.path.dirname(os.path.abspath(__file__))
 grandparent_folder_abspath = os.path.dirname(os.path.dirname(current_folder_abspath))
@@ -34,8 +37,8 @@ DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN_GPT")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 BOT_NAME = "chat-gpt"
 CHANNEL_ID = os.getenv("CHANNEL_ID_GPT")
-model_engine = "gpt-3.5-turbo-1106"
-# model_engines = ["gpt-3.5-turbo-1106","gpt-4-1106-preview","gpt-4-vision-preview"]
+model_engine = "gpt-3.5-turbo"
+# model_engines = ["gpt-3.5-turbo","gpt-4-turbo"]
 chat_log = []
 system_set = [
     {
@@ -157,21 +160,19 @@ async def gpt_delete(interaction: discord.Interaction):
     await interaction.response.send_message(response)
 
 
-@tree.command(name="gpt-switch", description="chat gptモデルをgpt-3.5-turbo-1106とgpt-4の間で切り替える")
+@tree.command(name="gpt-switch", description="chat gptモデルをgpt-3.5-turboとgpt-4-turboの間で切り替える")
 async def gpt_switch(interaction: discord.Interaction):
-    """switching the ChatGPT model between gpt-3.5-turbo-1106 and gpt-4.
+    """switching the ChatGPT model between gpt-3.5-turbo and gpt-4-turbo.
 
     Args:
         interaction (discord.Interaction): interaction.
     """
     logger.info("command: gpt-switch")
     global model_engine
-    if model_engine == "gpt-3.5-turbo-1106":
-        model_engine = "gpt-4-1106-preview"
-    elif model_engine == "gpt-4-1106-preview":
-        model_engine = "gpt-4-vision-preview"
-    elif model_engine == "gpt-4-vision-preview":
-        model_engine = "gpt-3.5-turbo-1106"
+    if model_engine == "gpt-3.5-turbo":
+        model_engine = "gpt-4-turbo"
+    elif model_engine == "gpt-4-turbo":
+        model_engine = "gpt-3.5-turbo"
     response = f"モデルエンジンを {model_engine} に変更しました。"
     logger.info("Change the model engine to " + model_engine)
     await interaction.response.send_message(response)
@@ -199,6 +200,156 @@ async def gpt_system(interaction: discord.Interaction, prompt: str):
     await interaction.response.send_message(response)
 
 
+def extract_text_from_pdf(file_path):
+    text = extract_text(file_path)
+    return text
+
+
+def clean_extracted_text(text):
+    text = text.replace("-\n", "")
+    text = re.sub(r"\s+", " ", text)
+    return text
+
+
+paper_chat_logs = {}
+
+
+@tree.command(name="gpt-paper-interpreter", description="論文の要約を行います。建てられたスレッド内で対話も可能です。")
+async def paper_interpreter(interaction: discord.Interaction, pdf_file: discord.Attachment):
+    """Summarizes the paper and starts a thread for further discussion.
+
+    Args:
+        interaction (discord.Interaction): The interaction object representing the user's request.
+        pdf_file (discord.Attachment): The PDF file to be summarized.
+
+    Returns:
+        None: The function does not return any value.
+    """
+    await interaction.response.defer()
+
+    logger.info("command: gpt-interpreter")
+    mydir = "/home/raspberrypi4/discord_bot/pdfs"
+    file_path = f"{mydir}/{pdf_file.filename}"
+    await pdf_file.save(file_path)
+
+    logger.info("saved " + file_path)
+
+    extracted_text = extract_text_from_pdf(file_path)
+    clean_text = clean_extracted_text(extracted_text)
+
+    # extractor = PaperMetaInfo()
+    # title, abstract = extractor.get_title_and_abstract(file_path)
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-4-turbo",  # "gpt-3.5-turbo-0125"
+        messages=[
+            {
+                "role": "user",
+                "content": "The opening sentence of the paper is given as following text. From the given text, extract the title, authors' names and abstract correctly. In particular, extract the abstract correctly without making a single mistake and without making any arrangements.\n\n###\n\n"
+                + clean_text[:16000],
+            }
+        ],
+        functions=[
+            {
+                "name": "get_metadata",
+                "description": "extract the title, authors' names and abstract correctly",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "title": {"type": "string", "description": "title"},
+                        "authors": {"type": "string", "description": "authors"},
+                        "abstract": {"type": "string", "description": "abstract"},
+                    },
+                    "required": ["title", "authors", "abstract"],
+                },
+            }
+        ],
+        function_call="auto",
+    )
+    response = json.loads(completion["choices"][0]["message"]["function_call"]["arguments"])
+    title = response["title"]
+    authors = response["authors"]
+    abstract = response["abstract"]
+    logger.info("assistant: Title: " + title)
+    logger.info("assistant: Authors: " + authors)
+    logger.info("assistant: Abstract: " + abstract)
+
+    # price = round_to_digits(
+    #     completion["usage"]["prompt_tokens"] * 0.50 / 1000000
+    #     + completion["usage"]["completion_tokens"] * 1.50 / 1000000,
+    #     3,
+    # )
+    price = round_to_digits(
+        completion["usage"]["prompt_tokens"] * 10.00 / 1000000
+        + completion["usage"]["completion_tokens"] * 30.00 / 1000000,
+        3,
+    )
+    logger.info(f"Usage: {price} USD")
+
+    message = await interaction.followup.send(f"**Title**: {title}")
+    thread = await interaction.channel.create_thread(
+        name=title if len(title) < 100 else title[:97] + "...", message=message, auto_archive_duration=60
+    )
+
+    response_list = [
+        f"**Authors**: {authors}",
+        f"**Abstract**: {abstract}",
+        f"(USAGE: {price} USD)",
+    ]
+    for response in response_list:
+        await thread.send(response)
+
+    content = f"""
+    Summarise the study's objectives, methodology, results and conclusions in Japanese using the following bullet points. However, parts that may be technical terms should also be written in English.
+    \n----------------------\n
+    {clean_text}
+    \n----------------------\n
+    研究目的、方法、結果、結論（と、あれば、今後のありうる研究の方向性）の要約:
+    """
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a researcher. You give thoughtful answers, taking into account the back and forth queries.",
+        },
+        {"role": "user", "content": content},
+    ]
+
+    message = await thread.send("生成中...")
+
+    retries = 3
+    while retries > 0:
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-4-turbo", messages=messages, request_timeout=300, max_tokens=2000
+            )
+            response = completion["choices"][0]["message"]["content"]
+            logger.info("assistant: " + completion["choices"][0]["message"].to_dict()["content"])
+            response_list = split_string(response)
+            price = round_to_digits(
+                completion["usage"]["prompt_tokens"] * 10.00 / 1000000
+                + completion["usage"]["completion_tokens"] * 30.00 / 1000000,
+                3,
+            )
+            logger.info(f"Usage: {price} USD")
+            response_list.append(f"(USAGE: {price} USD)")
+            messages.append(completion["choices"][0]["message"].to_dict())
+            await message.delete()
+            for response in response_list:
+                await thread.send(response)
+            break
+        except openai.error.Timeout as e:
+            retries -= 1
+            logger.exception(e)
+            # await thread.reply(
+            #     f"OpenAI APIでエラーが発生しました。\n{traceback.format_exception_only(e)}", mention_author=False
+            # )
+            await reply_openai_exception(retries, thread, e)
+
+    global paper_chat_logs
+    paper_chat_logs[thread.id] = messages
+
+
 @client.event
 async def on_message(message):
     """
@@ -215,12 +366,19 @@ async def on_message(message):
 
     """
     global chat_log
+    global paper_chat_logs
     global model_engine
     global total_token
     if message.author.bot:
         return
     if message.author == client.user:
         return
+    if (
+        message.channel.type == discord.ChannelType.public_thread
+        and message.channel.owner_id == client.user.id
+        and message.channel.id in paper_chat_logs.keys()
+    ):
+        await on_paper_thread(message)
     if str(message.channel.id) == CHANNEL_ID:
         msg = await message.reply("生成中...", mention_author=False)
         # async with message.channel.typing():
@@ -230,7 +388,7 @@ async def on_message(message):
             await message.channel.send("質問内容がありません")
             return
         content = prompt
-        if len(message.attachments) > 0 and model_engine == "gpt-4-vision-preview":
+        if len(message.attachments) > 0:
             for attachment in message.attachments:
                 if attachment.content_type.startswith("image"):
                     # 画像のダウンロード
@@ -255,8 +413,7 @@ async def on_message(message):
         while retries > 0:
             try:
                 input_chat_log = chat_log
-                if model_engine != "gpt-4-vision-preview":
-                    input_chat_log = system_set + chat_log
+                input_chat_log = system_set + chat_log
                 completion = openai.ChatCompletion.create(
                     model=model_engine, messages=input_chat_log, request_timeout=120, max_tokens=2000
                 )
@@ -264,23 +421,16 @@ async def on_message(message):
                 response_list = split_string(response)
                 chat_log.append(completion["choices"][0]["message"].to_dict())
                 logger.info("assistant: " + completion["choices"][0]["message"].to_dict()["content"])
-                if model_engine == "gpt-3.5-turbo-1106":
+                if model_engine == "gpt-3.5-turbo":
                     price = round_to_digits(
-                        completion["usage"]["prompt_tokens"] * 0.0010 / 1000
-                        + completion["usage"]["completion_tokens"] * 0.0020 / 1000,
+                        completion["usage"]["prompt_tokens"] * 0.50 / 1000000
+                        + completion["usage"]["completion_tokens"] * 1.50 / 1000000,
                         3,
                     )
-                elif model_engine == "gpt-4-1106-preview":
+                elif model_engine == "gpt-4-turbo":
                     price = round_to_digits(
-                        completion["usage"]["prompt_tokens"] * 0.01 / 1000
-                        + completion["usage"]["completion_tokens"] * 0.03 / 1000,
-                        3,
-                    )
-                    response_list.append(f"(USAGE: {price} USD)")
-                elif model_engine == "gpt-4-vision-preview":
-                    price = round_to_digits(
-                        completion["usage"]["prompt_tokens"] * 0.01 / 1000
-                        + completion["usage"]["completion_tokens"] * 0.03 / 1000,
+                        completion["usage"]["prompt_tokens"] * 10.00 / 1000000
+                        + completion["usage"]["completion_tokens"] * 30.00 / 1000000,
                         3,
                     )
                     response_list.append(f"(USAGE: {price} USD)")
@@ -315,6 +465,53 @@ async def on_message(message):
                     f"エラーが発生しました。\n{traceback.format_exception_only(e)}", mention_author=False
                 )
                 break
+
+
+async def on_paper_thread(message):
+    """Process the received message and generate a response.
+
+    Args:
+        message (discord.Message): The message object representing the received message.
+
+    Returns:
+        None
+    """
+    global paper_chat_logs
+    msg = await message.reply("生成中...", mention_author=False)
+    prompt = message.content
+    paper_chat_logs[message.channel.id].append({"role": "user", "content": prompt})
+    retries = 3
+    while retries > 0:
+        try:
+            completion = openai.ChatCompletion.create(
+                model="gpt-4-turbo",
+                messages=paper_chat_logs[message.channel.id],
+                request_timeout=240,
+                max_tokens=2000,
+            )
+            response = completion["choices"][0]["message"]["content"]
+            response_list = split_string(response)
+            paper_chat_logs[message.channel.id].append(completion["choices"][0]["message"].to_dict())
+            logger.info("assistant: " + completion["choices"][0]["message"].to_dict()["content"])
+            # model_engine == "gpt-4-turbo":
+            price = round_to_digits(
+                completion["usage"]["prompt_tokens"] * 0.01 / 1000
+                + completion["usage"]["completion_tokens"] * 0.03 / 1000,
+                3,
+            )
+            logger.info(f"Usage: {price} USD")
+            response_list.append(f"(USAGE: {price} USD)")
+            await msg.delete()
+            for response in response_list:
+                await message.channel.send(response)
+            break
+        except openai.error.Timeout as e:
+            retries -= 1
+            logger.exception(e)
+            # await thread.reply(
+            #     f"OpenAI APIでエラーが発生しました。\n{traceback.format_exception_only(e)}", mention_author=False
+            # )
+            await reply_openai_exception(retries, message, e)
 
 
 logger.info("Start client.")
